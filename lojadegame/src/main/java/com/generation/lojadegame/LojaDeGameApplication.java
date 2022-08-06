e9a8bd868dea9fe688eed92d62ec9de5529817ae
unknown <souza.nascimento.nathalia@gmail.com>
2022-08-06 12:47:35 -0300

    Atividade do bootcamp da generation

---
+++ lojadegame/src/main/java/com/generation/lojadegame/LojaDeGameApplication.java
@@ -0,0 +1,13 @@
+package com.generation.lojadegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeGameApplication.class, args);
+	}
+
+}
---
+++ lojadegame/src/main/java/com/generation/lojadegame/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.generation.lojadegame.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.lojadegame.model.ProdutoModel;
+
+public interface ProdutoRepository extends JpaRepository <ProdutoModel, Long>{
+	public List<ProdutoModel> findAllByPrecoBetween(@Param("inicio") BigDecimal inicio, @Param ("fim") BigDecimal fim );
+	
+	public List<ProdutoModel> findAllByItemContainingIgnoreCase(@Param ("item")String item);
+
+}
